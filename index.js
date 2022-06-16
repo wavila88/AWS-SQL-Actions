ced3d71cda9251e56385dc0470f3f72a433264c8
William Avila <willocoav@example.com>
2022-06-16 10:40:51 -0500

    version 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
   const res = await billingRepo.makeRequest()
   const response = {
       statusCode: 200,
-      body: 'continous integration Action succesfully version 2'+event.id,
+      body: 'continous integration Action succesfully version 3',
   };
   return response;
 };
